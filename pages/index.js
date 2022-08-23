f3600c111a65069e6b413d5d17e583823055799f
Jordin de León <jordindre77@outlook.com>
2022-08-23 16:52:02 -0600

    primeros avances

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
-  return (
-    <div >
-     Next js
-    </div>
-  )
+export default function Home({ categorias }) {
+  console.log(categorias)
+  return <div>Nextjs</div>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categorias = await prisma.categoria.findMany();
+  console.log(categorias);
+  return {
+    props: {
+      categorias,
+    },
+  };
+};
